2cca8abdd3c4f8749e23ea0e9d6386de90ee7d01
vdmkhrp <vdmkhrp@gmail.com>
2024-08-31 22:59:14 +0300

    create header and hero v0.5

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello!");
+const burgerBtn = document.querySelector(".burger-btn");
+const heroMenu = document.querySelector(".hero__menu");
+const overlay = document.querySelector(".overlay");
+
+burgerBtn.addEventListener("click", () => {
+  heroMenu.classList.add("hero__menu--active");
+  overlay.classList.add("overlay--active");
+});
+
+overlay.addEventListener("click", () => {
+  heroMenu.classList.remove("hero__menu--active");
+  overlay.classList.remove("overlay--active");
+});
