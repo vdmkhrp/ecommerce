cdeed24a3a7485026f3d62253743d244af95cb5f
vdmkhrp <vdmkhrp@gmail.com>
2024-09-04 02:27:44 +0300

    added swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,25 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
 const burgerBtn = document.querySelector(".burger-btn");
 const heroMenu = document.querySelector(".hero__menu");
 const overlay = document.querySelector(".overlay");
+
+const swiper = new Swiper('.swiper', {
+  effect: 'slide',
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  grabCursor: true,
+  autoHeight: true,
+  speed: 600,
+  spaceBetween: 8,
+  loop: true,
+  loopedSlides: 4,
+  slideToClickedSlide: true,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+});
 
 burgerBtn.addEventListener("click", () => {
   heroMenu.classList.add("hero__menu--active");
